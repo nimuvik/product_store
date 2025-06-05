9b30cb6ff61e076f1aa8de0dc6515736148cf620
nimuvik <nika.murash@gmail.com>
2025-06-06 00:31:34 +0300

    Update models.py

--- product_store/product_store/store/models.py
+++ product_store/product_store/store/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -20,6 +22,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Favorite(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
